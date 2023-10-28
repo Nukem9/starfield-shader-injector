4d7b0d429e5c5bdbdb672bc7109d4edee3ba15e7
Nukem <Nukem@outlook.com>
2023-10-28 11:05:01 -0400

    Use linear vectorized signature scans

--- source/Hooking/Offsets.cpp
+++ source/Hooking/Offsets.cpp
@@ -43,17 +43,35 @@ std::span<const uint8_t>::iterator Offsets::SignatureStorageWrapper::ScanRegion(
 	if (m_Signature.empty() || m_Signature.size() > Region.size())
 		return Region.end();
 
+	const auto nonWildcardSubrange = FindLongestRun();
+	const auto subrangeAdjustment = nonWildcardSubrange.data() - m_Signature.data();
+
+	if (nonWildcardSubrange.empty()) // if (all wildcards)
+		return Region.begin();
+
+	auto testFullSignature = [this](std::span<const uint8_t>::iterator Iter)
+	{
+		for (const auto& entry : m_Signature)
+		{
+			if (!entry.Wildcard && entry.Value != *Iter)
+				return false;
+
+			Iter++;
+		}
+
+		return true;
+	};
+
+	const auto scanStart = Region.begin() + subrangeAdjustment;					   // Seek forward to prevent underflow
+	const auto scanEnd = (Region.end() - m_Signature.size()) + subrangeAdjustment; // Seek backward to prevent overflow
+	auto pos = scanStart;
+
+#if 0
 	// Use a Boyer-Moore-Horspool search for each signature.
 	//
 	// While BMH itself doesn't support wildcards, we can still use the largest contiguous signature
 	// byte range that excludes wildcards, and then do a linear scan to match the rest.
-	const auto bmhSubrange = FindLongestRun();
-	const auto subrangeAdjustment = bmhSubrange.data() - m_Signature.data();
-
-	if (bmhSubrange.empty()) // if (all wildcards)
-		return Region.begin();
-
-	const auto lastByteIndex = static_cast<ptrdiff_t>(bmhSubrange.size() - 1);
+	const auto lastByteIndex = static_cast<ptrdiff_t>(nonWildcardSubrange.size() - 1);
 	const auto diff = std::max<ptrdiff_t>(lastByteIndex, 1);
 
 	// Prime the skip lookup table
@@ -61,30 +79,65 @@ std::span<const uint8_t>::iterator Offsets::SignatureStorageWrapper::ScanRegion(
 	skipLUT.fill(static_cast<uint8_t>(diff));
 
 	for (ptrdiff_t i = lastByteIndex - diff; i < lastByteIndex; i++)
-		skipLUT[bmhSubrange[i].Value] = static_cast<uint8_t>(lastByteIndex - i);
+		skipLUT[nonWildcardSubrange[i].Value] = static_cast<uint8_t>(lastByteIndex - i);
 
-	const auto scanStart = Region.begin() + subrangeAdjustment;						// Seek forward to prevent underflow
-	const auto scanEnd = (Region.end() - m_Signature.size()) + subrangeAdjustment;	// Seek backward to prevent overflow
-
-	for (auto pos = scanStart; pos <= scanEnd; pos += skipLUT[pos[lastByteIndex]])
+	for (; pos <= scanEnd; pos += skipLUT[pos[lastByteIndex]])
 	{
 		// Match the BMH-only subrange first, then run the full check if it succeeds
 		for (ptrdiff_t i = lastByteIndex; i >= 0; i--)
 		{
-			if (!bmhSubrange[i].Wildcard && bmhSubrange[i].Value != pos[i])
+			if (nonWildcardSubrange[i].Value != pos[i])
 				goto nextIter;
 		}
 
-		for (ptrdiff_t j = 0; j < std::ssize(m_Signature); j++)
-		{
-			if (!m_Signature[j].Wildcard && m_Signature[j].Value != pos[j - subrangeAdjustment])
-				goto nextIter;
-		}
-
-		return pos - subrangeAdjustment;
+		if (testFullSignature(pos - subrangeAdjustment))
+			return pos - subrangeAdjustment;
 
 	nextIter:;
 	}
+#else
+	// Linear vectorized search. Turns out CPUs are 2-3x faster at this than BMH.
+	//
+	// Unrolled version of http://0x80.pl/articles/simd-strfind.html#generic-sse-avx2 since AVX2 support
+	// can't be assumed. iterCount is used to avoid three extra branches per loop instead of comparing pos.
+	const ptrdiff_t perIterSize = sizeof(__m128i) * 2;
+	ptrdiff_t iterCount = (scanEnd - scanStart) / perIterSize;
+
+	const __m128i firstBlockMask = _mm_set1_epi8(nonWildcardSubrange.front().Value);
+	const __m128i lastBlockMask = _mm_set1_epi8(nonWildcardSubrange.back().Value);
+
+	for (; iterCount > 0; pos += perIterSize, iterCount--)
+	{
+		auto loadMask = [&](const size_t Offset)
+		{
+			const __m128i firstBlock = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&pos[Offset]));
+			const __m128i lastBlock = _mm_loadu_si128(reinterpret_cast<const __m128i *>(&pos[Offset + nonWildcardSubrange.size() - 1]));
+			const __m128i mask128 = _mm_and_si128(_mm_cmpeq_epi8(firstBlockMask, firstBlock), _mm_cmpeq_epi8(lastBlockMask, lastBlock));
+
+			return static_cast<uint32_t>(_mm_movemask_epi8(mask128)) << Offset;
+		};
+
+		auto mask = loadMask(0) | loadMask(sizeof(__m128i));
+
+		while (mask != 0)
+		{
+			// Get the index of the first set (leftmost) bit and clear it
+			unsigned long bitIndex;
+			_BitScanForward(&bitIndex, mask);
+
+			mask &= (mask - 1);
+
+			if (testFullSignature(pos + bitIndex - subrangeAdjustment))
+				return pos + bitIndex - subrangeAdjustment;
+		}
+	}
+
+	for (; pos <= scanEnd; pos++)
+	{
+		if (testFullSignature(pos - subrangeAdjustment))
+			return pos - subrangeAdjustment;
+	}
+#endif
 
 	return Region.end();
 }
