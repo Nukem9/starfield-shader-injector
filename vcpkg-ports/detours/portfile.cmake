9276b4e38c9c51eae41ab58c4e4041ac391b41a7
Nukem <Nukem@outlook.com>
2023-12-01 23:26:42 -0500

    Tweak detours port again

--- vcpkg-ports/detours/portfile.cmake
+++ vcpkg-ports/detours/portfile.cmake
@@ -39,6 +39,6 @@ if (NOT VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
     configure_file("${CMAKE_CURRENT_LIST_DIR}/detours-targets.cmake.in" "${CURRENT_PACKAGES_DIR}/share/${PORT}/detours-targets-release.cmake" @ONLY)
 endif()
 
-file(COPY "${CMAKE_CURRENT_LIST_DIR}/detoursConfig.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/detours-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.md")
